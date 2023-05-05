1d8f277cc3c52a29085786fba2e7929db0ae7547
sezer-muhammed <e230531@metu.edu.tr>
2023-05-05 17:48:33 +0300

    fix

---
+++ docs/$READTHEDOCS_OUTPUT/html/_sources/generated/eflatun_uav.rst.txt
@@ -0,0 +1,32 @@
+﻿eflatun\_uav
+============
+
+.. automodule:: eflatun_uav
+
+   
+   
+   
+
+   
+   
+   
+
+   
+   
+   
+
+   
+   
+   
+
+
+
+.. rubric:: Modules
+
+.. autosummary::
+   :toctree:
+   :recursive:
+
+   eflatun_uav.helpers
+   eflatun_uav.number_generators
+
--- docs/conf.py
+++ docs/conf.py
@@ -2,26 +2,26 @@ import os
 import sys
 import sphinx_rtd_theme
 from pathlib import Path
-
+print("debug 1")
 docs_path = Path(__file__).parent
 project_path = docs_path.parent
 
 sys.path.insert(0, str(project_path.resolve()))
-
+print("debug 1")
 for child_path in project_path.glob('**/'):
     if child_path.is_dir() and (child_path / '__init__.py').exists():
         sys.path.insert(0, str(child_path.resolve()))
-
+print("debug 1")
 project = 'Eflatun UAV'
 copyright = '2023, Muhammed Sezer, Şevval Dikkaya'
 author = 'Muhammed Sezer, Şevval Dikkaya'
 release = '2023'
-
+print("debug 1")
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+print("debug 1")
 extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon', 'sphinx.ext.autosummary',]
-
+print("debug 1")
 autodoc_default_options = {
     'members': True,
     'undoc-members': True,
@@ -30,7 +30,7 @@ autodoc_default_options = {
     'imported-members': True,
     'show-inheritance': True,
 }
-
+print("debug 1")
 
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
@@ -42,11 +42,11 @@ napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
-
+print("debug 1")
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+print("debug 1")
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
