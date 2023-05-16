b3d469dcbab786a74b64eceab7a539cdb1587e9e
sezer-muhammed <e230531@metu.edu.tr>
2023-05-16 09:51:05 +0300

    basefilter class predict and update added

--- eflatun_uav/filters/__init__.py
+++ eflatun_uav/filters/__init__.py
@@ -2,6 +2,7 @@
 """
 
 from typing import List
+import numpy as np
 
 class BaseFilter():
     """Base Filter object for Filter module
@@ -16,4 +17,25 @@ class BaseFilter():
         """
         self.input_size = input_size
         self.output_size = output_size
-        
+
+    def update(self, input_state: np.ndarray):
+        """_summary_
+
+        Args:
+            input_state (np.ndarray): _description_
+
+        Raises:
+            NotImplementedError: _description_
+        """
+        raise NotImplementedError("This is base class, please refer the main classes")
+
+    def predict(self) -> np.ndarray:
+        """_summary_
+
+        Raises:
+            NotImplementedError: _description_
+
+        Returns:
+            np.ndarray: _description_
+        """
+        raise NotImplementedError("This is base class, please refer the main classes")
